f105b23a3d73d33a6db63a86f1e298b93516244e
arash-aloha <arash.a@live.se>
2021-09-23 08:35:34 +0100

    change of folder structures

--- src/database.js
+++ src/database.js
@@ -2,6 +2,15 @@ const admin = require("firebase-admin");
 const serviceAccount = require('./secrets/firebase-key.json');
 
 
+let privateKey;
+
+if( process.env.PRIVATE_KEY ) {
+    privateKey = JSON.parse(process.env.PRIVATE_KEY)
+} else {
+    privateKey = require('./secrets/firebase-key.json')
+}
+
+
 function connect() {
     admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -10,13 +19,5 @@ function connect() {
     return db;
 }
 
-/*
-let privateKey;
-if( process.env.PRIVATE_KEY ) {
-    privateKey = JSON.parse(process.env.PRIVATE_KEY)
-} else {
-    privateKey = require('./example-private-key.json)
-}
-*/
 
 module.exports = { connect }
